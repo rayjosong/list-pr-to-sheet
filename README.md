b83ffd79280188a5a88da1b419c914ac47cf6ffe
Raymond Ong <rayjosong@gmail.com>
2023-11-26 17:27:23 +0800

    completed: refactored, add progressBar CLI and README.md

---
+++ README.md
@@ -0,0 +1,57 @@
+# GitHub PR Fetcher 
+
+This is a simple Node.js script to automatically fetch Pull Request (PR) data from a specific GitHub repository, and export the data to an Excel spreadsheet. 
+
+## Prerequisites
+
+Before you begin, ensure you have met the following requirements:
+
+* You have installed Node.js and npm. If not, follow the instructions from this link: [Node.js](https://nodejs.org/en/download/).
+* You have a GitHub API token. Follow these instructions to generate one: [Creating a personal access token](https://docs.github.com/en/authentication/keeping-your-account-and-data-secure/creating-a-personal-access-token)
+
+## Setup and Installation
+
+1. Clone this repository to your local system
+    ```
+   git clone <repository-url>
+    ```
+2. Navigate into the project directory, and install the required packages
+    ```
+   cd <project-dir>
+   npm install
+    ```
+
+## Environment Variables
+
+To run this project, you need to add the following environment variables in a `.env` file in your project directory.
+
+    ```
+   REPO_NAME=your_repo_name
+   ORGANISATION_NAME=your_organisation_name
+   GITHUB_API_TOKEN=your_github_api_token
+   USERNAME=github_username
+    ```
+
+Replace the placeholder values (e.g., `your_repo_name`) with actual values. Note that:
+* `REPO_NAME` is the name of the repository from which you want to fetch PRs
+* `ORGANISATION_NAME` is the name of the organization that the repository belongs to
+* `GITHUB_API_TOKEN` is your personal GitHub API token
+* `USERNAME` is your GitHub username, PRs from this user will be fetched
+
+## Run the Script
+
+After setting up, run the following command to fetch and store the PR data:
+
+    ```
+   node filename.js
+    ```
+
+Replace `filename.js` with the actual filename of the script. 
+
+The script will fetch the PR data and save it to an Excel spreadsheet named `{repo}_{user}_PRs.xlsx`. 
+
+Quick tip: you can use the "HYPERLINK=(url, label)" function on excel if you want to combine the `Title` and `URL` columns in the sheet.
+
+## Contributing
+
+If you want to contribute to this project, feel free to fork the repository, make your changes and create a pull request. Contributions are always welcomed
--- script.js
+++ script.js
@@ -1,70 +1,9 @@
-// const axios = require("axios");
-// const { Parser } = require("json2csv");
-// const readline = require("readline");
-// const fs = require("fs");
-//
-// const rl = readline.createInterface({
-//   input: process.stdin,
-//   output: process.stdout,
-// });
-//
-// function askQuestion(query) {
-//   return new Promise((resolve) => rl.question(query, resolve));
-// }
-//
-// async function main() {
-//   const organization = await askQuestion(
-//     "Please enter your organisation name: ",
-//   );
-//
-//   const repo = await askQuestion("Please enter the repository name: ");
-//   const token = await askQuestion("Please enter your access token: ");
-//
-//   const url = `https://api.github.com/repos/${organization}/${repo}/pulls?state=all`;
-//   const options = {
-//     headers: {
-//       Authorization: `token ${token}`,
-//       "User-Agent": "rayjosong", // change accordingly
-//       Accept: "application/vnd.github.v3+json",
-//     },
-//   };
-//
-//   try {
-//     const response = await axios.get(url, options);
-//
-//     const pulls = response.data.map((pull) => ({
-//       number: pull.number,
-//       title: pull.title,
-//       user: pull.user.login,
-//       state: pull.state,
-//       created_at: pull.created_at,
-//       updated_at: pull.updated_at,
-//       closed_at: pull.closed_at,
-//       additions: pull.additions,
-//       deletions: pull.deletions,
-//     }));
-//
-//     const parser = new Parser();
-//     const csv = parser.parse(pulls);
-//
-//     console.log("writing to " + `${repo}_pull_requests.csv`);
-//     fs.writeFileSync(`${repo}_pull_requests.csv`, csv);
-//   } catch (error) {
-//     console.error("error: " + error);
-//   }
-//
-//   rl.close();
-// }
-//
-// main();
-//
 require("dotenv").config();
 const axios = require("axios");
 const readline = require("readline-sync");
+const xlsx = require("xlsx");
 
-// Ask for repo and organisation
-// const repo = readline.question("Please enter the repository name: ");
-// const org = readline.question("Please enter the organisation name: ");
+const ProgressBar = require("progress");
 
 const repo = process.env.REPO_NAME;
 const org = process.env.ORGANISATION_NAME;
@@ -82,40 +21,96 @@ const headers = {
 };
 
 let page = 1;
-let userPRs = [];
-
 let noUserPRsCount = 0;
+let bar = null;
 
-const fetchPRs = async () => {
+const getPRSize = (additions, deletions) => {
+  const totalChanges = additions + deletions;
+  if (totalChanges < 10) return "XS";
+  if (totalChanges < 50) return "S";
+  if (totalChanges < 200) return "M";
+  if (totalChanges < 1000) return "L";
+  return "XL";
+};
+
+const fetchPRs = async (totalPRs = 100) => {
+  if (bar === null) {
+    bar = new ProgressBar("Fetching PRs [:bar] :current/:total \t", {
+      total: totalPRs,
+    });
+  }
   try {
-    console.log(
-      `Fetching page ${page}. Number of PRs associated to ${user}: ${userPRs.length}`,
-    );
+    console.log(`\nFetching page ${page}\n`);
     const response = await axios.get(
-      `https://api.github.com/repos/${org}/${repo}/pulls?state=all&page=${page}&per_page=100`,
+      `https://api.github.com/repos/${process.env.ORGANISATION_NAME}/${process.env.REPO_NAME}/pulls?state=closed&page=${page}&per_page=100`,
       { headers },
     );
-    const prs = response.data;
-    const filteredPRs = prs.filter((pr) => pr.user.login === user);
-    userPRs = [...userPRs, ...filteredPRs];
 
-    // If the response is not empty and there are PRs associated with the user, there might be more pages
-    if (filteredPRs.length > 0) {
-      noUserPRsCount = 0; // reset the counter if PRs associated with the user are found
-      page++;
-      await fetchPRs();
-    } else if (prs.length > 0 && noUserPRsCount <= 3) {
-      noUserPRsCount++; // increment the counter if no PRs associated with the user are found
-      page++;
-      await fetchPRs();
+    const prs = response.data;
+    const userPRs = prs.filter((pr) => pr.user.login === process.env.USERNAME);
+
+    for (let pr of userPRs) {
+      const prDetails = await axios.get(pr.url, { headers });
+      const { additions, deletions, comments, body } = prDetails.data;
+
+      pr.size = getPRSize(additions, deletions);
+      pr.comments = comments;
+      pr.body = body;
+      bar.tick();
+      console.log(
+        `#${pr.number} ${pr.title} (Size: ${pr.size}, Comments: ${pr.comments})`,
+      );
+    }
+
+    if (noUserPRsCount === 3) return userPRs;
+
+    if (userPRs.length === 0) {
+      noUserPRsCount++;
     } else {
-      // if there are more than 3 pages where no PRs associated to user are found, then assume it was before the join date of the user
-      console.log(`Found ${userPRs.length} pull requests by ${user}:`);
-      userPRs.forEach((pr) => console.log(`#${pr.number}: ${pr.title}`));
+      noUserPRsCount = 0;
+    }
+
+    page++;
+
+    if (userPRs.length < totalPRs) {
+      bar.interrupt(`Fetching more PRs...`);
+      return [...userPRs, ...(await fetchPRs())];
     }
   } catch (error) {
     console.error(`Could not fetch pull requests: ${error.message}`);
   }
 };
 
-fetchPRs();
+const writePRsToSpreadsheet = (userPRs) => {
+  const prData = userPRs.map((pr) => ({
+    "PR Number": pr.number,
+    Title: pr.title,
+    URL: pr.html_url,
+    "Created At": pr.created_at,
+    "Closed At": pr.closed_at,
+    "Merged At": pr.merged_at,
+    Size: pr.size,
+    Comments: pr.comments,
+    Body: pr.body,
+  }));
+
+  const ws = xlsx.utils.json_to_sheet(prData, {
+    header: [
+      "PR Number",
+      "Title",
+      "URL",
+      "Created At",
+      "Closed At",
+      "Merged At",
+      "Size",
+      "Comments",
+      "Body",
+    ],
+  });
+
+  const wb = xlsx.utils.book_new();
+  xlsx.utils.book_append_sheet(wb, ws, "PRs");
+  xlsx.writeFile(wb, `${repo}_${user}_PRs.xlsx`);
+};
+
+fetchPRs().then(writePRsToSpreadsheet);
